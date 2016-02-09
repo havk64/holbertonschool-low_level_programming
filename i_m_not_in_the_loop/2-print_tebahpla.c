c7fa31eeab13c77605ba4cd10dcc742a6814b2fb
havk64 <havk.fs@gmail.com>
2016-02-09 01:59:51 +0000

    adding comment to task 2

--- i_m_not_in_the_loop/2-print_tebahpla.c
+++ i_m_not_in_the_loop/2-print_tebahpla.c
@@ -1,5 +1,6 @@
 #include "my_functions.h"
 
+/* The funcion print_tebahpla accepts no argument and iterate through each letter of alphabet in reverse order(starting by 'z') and printing each letter using the print_char function. */
 void print_tebahpla(void)
 {
 	char c;
