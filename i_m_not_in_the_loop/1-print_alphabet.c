e535acc264503f7d8a649a5b07392350f06ffdeb
Alexandro de Oliveira <havk.fs@gmail.com>
2016-02-08 17:40:59 -0800

    adding comments to task 1 file

--- i_m_not_in_the_loop/1-print_alphabet.c
+++ i_m_not_in_the_loop/1-print_alphabet.c
@@ -1,5 +1,7 @@
 #include "my_functions.h"
 
+/*Function print_alphabet that accepts no argument and loop through each letter of alphabet
+printing each letter utilizing the print_char function.*/
 void print_alphabet(void)
 {
 	char c;
