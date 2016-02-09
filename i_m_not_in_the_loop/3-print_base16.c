5434bab07478e8718fa550812b1ebf38d887987c
havk64 <havk.fs@gmail.com>
2016-02-09 02:07:44 +0000

    adding comment to the task 3 file

--- i_m_not_in_the_loop/3-print_base16.c
+++ i_m_not_in_the_loop/3-print_base16.c
@@ -1,15 +1,16 @@
 #include "my_functions.h"
 
+/* The function print_base16 accepts no argument and prints all the numbers of base 16 */
 void print_base16(void)
 {
 	int i;
 
- 	for (i=0;i<16;i++)
+ 	for (i=0;i<16;i++)	# Iterate 16 times in acending order.
 	{
  	if (i<10)
-	print_char(((char) i)+48);
+	print_char(((char) i)+48); # adds 48 to first 10 numbers to get right ascii characters.
 	else
-	print_char(((char) i)+55);
+	print_char(((char) i)+55); # adds 55 to remaining numbers to get right ascii characters.
 	}
 }
 
