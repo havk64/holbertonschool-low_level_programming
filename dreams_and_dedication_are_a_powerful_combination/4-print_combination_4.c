19a3056c4fc63cc54395629e96275a98a08ce139
havk64 <havk.fs@gmail.com>
2016-02-14 23:20:10 -0800

    Task 4 done

--- dreams_and_dedication_are_a_powerful_combination/4-print_combination_4.c
+++ dreams_and_dedication_are_a_powerful_combination/4-print_combination_4.c
@@ -1,5 +1,28 @@
 #include "my_functions.h"
-void print_combination_4(void)
-{
-	
-}
+/*=> This function prints - on one line - all possible combinations
+  => of two two-digit numbers. */
+void print_combination_4(void){
+    
+    int i,j, k,l,a,b;
+    for(i = 0;i <= 9;i++){
+        for (j = 0;j <= 9;j++){
+            for (k = 0;k <= 9; k++){
+                for (l = 0;l <= 9; l++){
+                    a = i * 10 + j;
+                    b = k * 10 + l;
+                    if(a<b){
+                        print_char(i+48);
+                        print_char(j+48);
+                        print_char(' ');
+                        print_char(k+48);
+                        print_char(l+48);
+                        if(  a!=98 ){
+                            print_char(',');
+                            print_char(' ');
+                        }
+                    }
+                }
+            }
+        }
+    }
+} 
