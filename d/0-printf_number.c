fd49cafc796963afa849adc5152668cdb6993839
havk64 <havk.fs@gmail.com>
2016-02-14 23:00:22 -0800

    Adding comment to 0-printf file

--- d/0-printf_number.c
+++ d/0-printf_number.c
@@ -1,6 +1,6 @@
-#include "my_functions.h"
 #include <stdio.h>
-
+#include "my_functions.h"
+/*=> This function takes an integer in parameter and prints it */
 void printf_number(int n)
 {
 	printf("%d", n);
