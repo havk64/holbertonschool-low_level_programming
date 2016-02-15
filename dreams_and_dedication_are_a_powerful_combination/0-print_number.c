d9df0d82e566f9c7670cd9769f4f4d6990ab4f51
havk64 <havk.fs@gmail.com>
2016-02-15 02:48:55 +0000

    Task 0 solved

--- dreams_and_dedication_are_a_powerful_combination/0-print_number.c
+++ dreams_and_dedication_are_a_powerful_combination/0-print_number.c
@@ -1,32 +1,32 @@
 #include "my_functions.h"
 
-void print_number(int n)
+void print_number(int n) /*=> Control flow to use function print accordingly */
 {  
-    if (n == 0){
-        print_char(48);
-    }
-    else if( n > 0){
-        print(n);
-    }
-    else {
-        print_char('-');
-        print((n * -1));
-    }
+	if (n == 0){     /*=> If n is zero prints zero */
+       		print_char(48);
+	}
+	else if( n > 0){
+        	print(n);
+	}
+	else {		/*=> If n is negative converts to absotulte value */
+		print_char('-');
+		print((n * -1)); 
+	}
 
 }
-void print(int n)
+void print(int n)        /*=> Uses the print_char to print each digit */
 {
-    int number=n;
-    int power=1;
-    int print;
-    while (number > 9){
-        number /= 10;
-        power *= 10;
-    }
-    while (n > 0){
-        print = n / power;
-        n %= power;
-        power /= 10;
-        print_char(print + 48);
-  }    
+	int number=n;
+	int power=1;
+	int print;
+	while (number > 9){  /*=> Finds the lenght of decimal  */
+		number /= 10;
+		power *= 10;
+	}
+	while (n > 0){ 	 /*=> Finds each digit and prints it with print_char */
+		print = n / power;
+		n %= power;
+		power /= 10;
+		print_char(print + 48);
+	}
 }
