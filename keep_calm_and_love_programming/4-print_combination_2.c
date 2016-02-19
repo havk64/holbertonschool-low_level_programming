6e13463606acd55db2c5b867edbcd0aa5396cec6
havk64 <havk.fs@gmail.com>
2016-02-19 12:54:57 -0800

    Task 4 solved

---
+++ keep_calm_and_love_programming/4-print_combination_2.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "my_functions.h"
+
+void print_combination_2(void)
+{
+	int i;
+	int j;
+	for(i = 0; i < 9; i++) {
+		for(j = 1 + i; j <= 9; j++) {
+			print_char(i+'0');
+			print_char(j+'0');
+			if(i < 8){
+				print_char(',');
+				print_char(' ');
+			}
+		}
+	}
+}
