ecc6851d71fdde194eabb39de6236389fefc4579
havk64 <havk.fs@gmail.com>
2016-02-19 11:34:55 -0800

    Task 3 solved

--- keep_calm_and_love_programming/3-print_to_98.c
+++ keep_calm_and_love_programming/3-print_to_98.c
@@ -1,8 +1,20 @@
-int print_char(char c);
+#include <stdio.h>
+
 void print_to_98(int n)
 {
 	int i;
-	for(i = n; n <=98; i++) {
-		print_char(n+48);
-	}
+	if(n <= 98)
+		for(i = n; i <=98; i++) {
+			if(i == 98)
+                        	printf("%d\n", i);
+			else
+				printf("%d, ", i);
+		}
+	else
+		for(i=n; i >= 98; i--)
+			if(i == 98)
+                                printf("%d\n", i);
+			else
+				printf("%d, ", i);
+
 }
