86d6d3c0576c5901a0593c833f19bed1cce932ab
havk64 <havk.fs@gmail.com>
2016-02-27 06:43:53 +0000

    Minor changes comments

--- i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
+++ i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
@@ -1,27 +1,25 @@
 #include "my_functions.h"
 #include <unistd.h>
-/*Print Char, accepts a character and prints it.*/
-int print_char(char c){
+/*Print Char: accepts a character and prints it.*/
+int print_char(char c) {
 	return(write(1, &c, 1));
 }
-/*Print Stars, accepts in integer and prints an odd number of starts(*)i.*/
-void print_stars(int n)
-{
+/*Print Stars: accepts in integer and prints an odd number of starts(*)i.*/
+void print_stars(int n) {
 	int number;
-	number = n + n +1;
-	while(number){
+	number = n + n + 1;
+	while(number) {
 		print_char('*');
 		number--;
 	}
 }
-/*Print Triangle, print each triangle with n numbers of lines, where n is the argument.*/
-void print_triangle(int n)
-{
+/*Print Triangle: print each triangle with n numbers of lines, where n is the argument.*/
+void print_triangle(int n) {
 	int i, j, number;
 	number = n;
-	for( i = 0; i < n ; i++){
+	for(i = 0; i < n ; i++) {
 		number--;
-		for(j = 0; j < number; j++){
+		for(j = 0; j < number; j++) {
 			print_char(' ');
 		}
 		print_stars(i);
@@ -31,8 +29,7 @@ void print_triangle(int n)
 /* Uses the print_triangle to prints the triangle n times, each triangle with h lines.*/
 void triangles_in_the_term(int h, int n)
 {
-	while(n)
-	{
+	while(n) {
 		print_triangle(h);
 		n--;
 	}
