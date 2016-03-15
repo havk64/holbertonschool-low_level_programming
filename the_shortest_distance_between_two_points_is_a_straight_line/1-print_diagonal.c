a12e4a7aefd23c996f5a6a358f7b923c85652e6a
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-03-15 18:54:22 +0000

    Task 1 completed

---
+++ the_shortest_distance_between_two_points_is_a_straight_line/1-print_diagonal.c
@@ -0,0 +1,27 @@
+int print_char(char c);
+
+/* This function just print spaces. */
+void print_spaces(int n)
+{
+  while(n) {
+    print_char(' ');
+    n--;
+  }
+}
+
+/* This function print diagonals because diagonals are fun! ;-) */
+void print_diagonal(int n)
+{
+  int i;
+  i = 0;
+  if(n <= 0) {
+    print_char('\n');
+  }
+  while(n > 0) {
+    print_spaces(i);
+    print_char(92);
+    print_char('\n');
+    i++;
+    n--;
+  }
+}
