80a89ba58769e14b8e66d525b023e5df33414639
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-03-16 16:39:12 +0000

    Adding comments

--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/2-string_copy.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/2-string_copy.c
@@ -1,3 +1,7 @@
+/* ======================================================== *
+ * This function just copy a string and returns its pointer *
+ * =========================================================*/
+
 char *string_copy(char *dest, const char *src)
 {
   char *cdest;
