3d4c9ec529282c7913cf7190201be6d8fa7b9be6
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-03-16 18:36:13 +0000

    Shrinking the code

--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/3-string_ncopy.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/3-string_ncopy.c
@@ -7,17 +7,12 @@ char *string_ncopy(char *dest, const char *src, int n)
   char *cdest;
   cdest = dest;
   while(n && *src) {
-    *dest = *src;
-    dest++; src++;
+    *dest++ = *src++;
     n--;
-    /*if(*src == 0)
-      break;*/
   }
   while(n) {
-    *dest = 0;
-    dest++;
+    *dest++ = 0;
     n--;
   }
-  /**dest = 0;*/
   return cdest;
 }
