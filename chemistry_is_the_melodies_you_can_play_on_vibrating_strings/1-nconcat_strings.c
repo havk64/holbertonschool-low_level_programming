05025c30fbc774eed912ae2d2619678de347804b
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-03-16 06:00:33 +0000

    Task 1 solved

---
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/1-nconcat_strings.c
@@ -0,0 +1,21 @@
+/* This function is similar to strncat() and used to concatenate two strings. */
+char *nconcat_strings(char *dest, const char *src, int n)
+{
+	char *cdest;
+	cdest = dest;
+	
+	while( *dest ) {
+		dest++;
+	}
+	while(n) {
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+		if(*src == 0) {
+			break;
+		}
+	}
+	return cdest;
+}
+ 
