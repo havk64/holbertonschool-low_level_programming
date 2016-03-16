cf01d26aa903f7dd3c6605d5b42408aa20dfd3cf
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-03-16 05:30:03 +0000

    Adding comments, alternative code

--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/0-contact_strings.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/0-contact_strings.c
@@ -3,11 +3,31 @@ int print_char(char c);
 /* This function concatenates two strings. */
 char *concat_strings(char *dest, const char *src)
 {
-  char *rdest = dest;
+  char *cdest = dest;
+
+  while (*dest) {
+    dest++;
+  }
+  while(*src) {
+    *dest = *src;
+    src++;
+    dest++;
+  }
+  return cdest;
+}
+
+/* ===============================================
+Or... more compactable, less readable: 
+
+char *concat_strings(char *dest, const char *src)
+{
+  char *cdest = dest;
 
   while (*dest)
     dest++;
   while ((*dest++ = *src++))
     ;
-  return rdest;
+  return cdest;
 }
+
+================================================== */
