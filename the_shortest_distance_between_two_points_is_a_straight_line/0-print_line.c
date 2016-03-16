ac8fe6f39c78a9ca9c8eb8d2afbe475a43594ba1
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-03-16 15:22:13 +0000

    Identation

--- the_shortest_distance_between_two_points_is_a_straight_line/0-print_line.c
+++ the_shortest_distance_between_two_points_is_a_straight_line/0-print_line.c
@@ -3,9 +3,9 @@ int print_char(char c);
 /* This function print "n" number of '_' characters, followed by a new line.*/
 void print_line(int n)
 {
-  while(n > 0) {
-    print_char('_');
-    n--;
-  }
-  print_char('\n');
+	while(n > 0) {
+		print_char('_');
+		n--;
+	}
+	print_char('\n');
 }
