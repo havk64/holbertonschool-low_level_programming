6b4daf788be8230f925935f117fb637ca5764b2a
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-03-17 06:25:05 +0000

    Indenting in K&R style

--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/2-string_copy.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/2-string_copy.c
@@ -4,10 +4,10 @@
 
 char *string_copy(char *dest, const char *src)
 {
-  char *cdest;
-  cdest = dest;
-  while((*dest++ = *src++))
-    ;
-  *dest = 0;
-  return cdest;
+    char *cdest;
+    cdest = dest;
+    while((*dest++ = *src++))
+	;
+    *dest = 0;
+    return cdest;
 }
