22e97c58140e03b43442d6e8cb43a6f04f1435d2
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-03-17 06:01:51 +0000

    Indenting in K&R Style

--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/print_array.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/print_array.c
@@ -1,54 +1,54 @@
 #include <unistd.h>
 #include <limits.h>
-/* ======================================================== *
- *             Function print_char()                        *
- * =========================================================*/
+/* =========================================================== *
+ *                Function print_char()                        *
+ * ============================================================*/
 int print_char(char c)
 {
-  return (write(1, &c, 1));
+    return (write(1, &c, 1));
 }
 
-/* ======================================================== *
- * Function print() uses print_char() to print each digit   *
- * =========================================================*/
+/* =========================================================== *
+ *    Function print() uses print_char() to print each digit   *
+ * ============================================================*/
 void print(int n)
 {
-  int number, power, digit;
-  number = n;
-  power = 1;
+    int number, power, digit;
+    number = n;
+    power = 1;
 
-  while (number > 9)     /*=> Finds the lenght of decimal  */
+    while (number > 9)     /*=> Finds the lenght of decimal    */
     {
-      number /= 10;
-      power *= 10;
+	number /= 10;
+	power *= 10;
     }
-  while (n > 0)          /*=> Finds each digit and prints it with print_char */
+    while (n > 0) /*=> Finds each digit,prints with print_char */
     {
-      digit = n / power;
-      n %= power;
-      power /= 10;
-      print_char(digit + '0');
+	digit = n / power;
+	n %= power;
+	power /= 10;
+	print_char(digit + '0');
     }
 }
 
-/* ======================================================== *
- *         Function print_number() uses print()             *
- * =========================================================*/
+/* =========================================================== *
+ *            Function print_number() uses print()             *
+ * ============================================================*/
 void print_number(int n) /*=> Control flow to use function print accordingly */
 {  
-  if (n == INT_MIN) {
-    n /= -10;
-  }
-  if (n == 0) {          /*=> If n is zero prints zero */
-    print_char('0');
-  }
-  else if( n > 0) {
-    print(n);
-  }
-  else {                 /*=> If n is negative converts to absotulte value */
-    print_char('-');
-    print(-n ); 
-  }
+    if (n == INT_MIN) {
+	n /= -10;
+    }
+    if (n == 0) {          /*=> If n is zero prints zero */
+	print_char('0');
+    }
+    else if( n > 0) {
+	print(n);
+    }
+    else {                 /*=> If n is negative converts to absotulte value */
+	print_char('-');
+	print(-n ); 
+    }
 }
 
 /* =========================================================================== *
@@ -57,16 +57,15 @@ void print_number(int n) /*=> Control flow to use function print accordingly */
  * =========================================================================== */
 void print_array(int *a, int n)
 {
-  int i;
-  i = 0;
+    int i;
+    i = 0;
   
-  while(i < n) {
-    if(i != 0) {
-      print_char(',');
-      print_char(' ');
+    while(i < n) {
+	if(i != 0) {
+	    print_char(',');
+	    print_char(' ');
+	}
+	print_number(*(a + i));
+	i++;
     }
-    print_number(*(a + i));
-    i++;
-  }
-  print_char('\n');
-}
+    print_char('\n');
