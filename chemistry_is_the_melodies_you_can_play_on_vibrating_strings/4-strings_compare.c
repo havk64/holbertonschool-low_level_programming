9f8139cfbc13445a44b4b79bebfd756f93a7fa62
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-03-17 05:06:14 +0000

    Improving indentation

--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/4-strings_compare.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/4-strings_compare.c
@@ -3,11 +3,11 @@
  * ============================================================= */
 int strings_compare(char *s1, const char *s2)
 {
-  while(*s1 == *s2) {
-    if(*s1 == 0) {
-      return 0;
-    }
-    s1++; s2++;
-  }
-  return *(const unsigned char *)s1 - *(const unsigned char *)s2;
+	while(*s1 == *s2) {
+		if(*s1 == 0) {
+			return 0;
+		}
+		s1++; s2++;
+	}
+	return *(const unsigned char *)s1 - *(const unsigned char *)s2;
 }
