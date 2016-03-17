ee00be9f3744261f6dfcaef878dbb03a71804269
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-03-17 06:32:26 +0000

    Indenting in K&R style

--- the_shortest_distance_between_two_points_is_a_straight_line/1-print_diagonal.c
+++ the_shortest_distance_between_two_points_is_a_straight_line/1-print_diagonal.c
@@ -5,16 +5,16 @@ int print_char(char c);
  * ============================================================ */
 void print_diagonal(int n)
 {
-  int i, j;
-  i = 0;
-  if(n <= 0) {
-    print_char('\n');
-  }
-  while(n-- > 0) {
-    j = i++;
-    while(j--)
-       print_char(' ');
-    print_char('\\');
-    print_char('\n');
-  }
+    int i, j;
+    i = 0;
+    if(n <= 0) {
+	print_char('\n');
+    }
+    while(n-- > 0) {
+	j = i++;
+	while(j--)
+	    print_char(' ');
+	print_char('\\');
+	print_char('\n');
+    }
 }
