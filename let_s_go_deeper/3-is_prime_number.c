962b19cbe227afe7fa0b5661f25598fd3c08c161
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-03-30 22:41:46 +0000

    Readability, compacting

--- let_s_go_deeper/3-is_prime_number.c
+++ let_s_go_deeper/3-is_prime_number.c
@@ -3,12 +3,9 @@
  * =================================================================== */
 int findit(int n, int i)
 {
-    if(n < 2)
-	return (0);
-    if(i > n/2)
-	return (1);
-    if(n % i == 0)
-	return (0);
+    if(n < 2) return (0);
+    if(i > n/2) return (1);
+    if(n % i == 0) return (0);
     return findit(n, i+1);
 }
 
