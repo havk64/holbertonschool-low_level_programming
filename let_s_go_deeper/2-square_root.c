95e88c07d3708ea160bfc70c051a1e26318115f2
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-03-30 18:02:56 +0000

    Task 2 completed

---
+++ let_s_go_deeper/2-square_root.c
@@ -0,0 +1,16 @@
+int findit(int n, int i)
+{
+    if(n < 0)
+	return (-1);
+    if(i * i == n)
+	return (i);
+    if(i * i > n)
+	return (-1);
+    return findit(n, i + 1);
+}
+
+
+int square_root(int n)
+{
+    return findit(n, 1);
+}
