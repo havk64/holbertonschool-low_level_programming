2c7ac595e4e7ee31018981006a4e355ff992f13f
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-03-30 16:11:04 +0000

    Task 0 completed

---
+++ let_s_go_deeper/0-factorial.c
@@ -0,0 +1,13 @@
+
+/* =================================================================== *
+ *            Returns the factorial of a given number                  *
+ * =================================================================== */
+int factorial(int n)
+{
+    if(n < 0)
+	return (-1);
+    if(n == 0)
+	return (1);
+    else
+	return( n * factorial(n-1));
+}
