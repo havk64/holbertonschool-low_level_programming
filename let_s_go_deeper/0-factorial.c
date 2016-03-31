3e53ec4bcb4fd8e65a27f52c3aee1f1e96655f7a
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-03-30 17:39:25 -0700

    Treating limits task 0

--- let_s_go_deeper/0-factorial.c
+++ let_s_go_deeper/0-factorial.c
@@ -1,10 +1,12 @@
-
+#include <limits.h>
 /* =================================================================== *
  *            Returns the factorial of a given number                  *
  * =================================================================== */
 int factorial(int n)
 {
-    if(n < 0) return (-1);
+    long unsigned limit = n;
+    limit *= n - 1;
+    if(n < 0 || limit > INT_MAX) return (-1);
     if(n == 0) return (1);
     return( n * factorial(n-1));
 }
