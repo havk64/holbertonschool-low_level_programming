ebffb4b7fd4b32b9011ab8bf15046939f1e352f1
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-04-04 19:23:47 -0700

    Improving comments

--- neither_irony_or_sarcasm_is_argument/0-nargs.c
+++ neither_irony_or_sarcasm_is_argument/0-nargs.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /* ========================================================= *
- * This function prints the number of arguments given to it. *
+ *    The function print_char prints just one charactere.    *
  * ========================================================= */
 int print_char(char c)
 {
     return(write(1,&c,1));
 }
 
+/* ========================================================= *
+ * This function prints the number of arguments given to it. *
+ * ========================================================= */
 int main(int argc, char __attribute__((unused)) *argv[]) {
     print_char( argc - 1 + '0');
     print_char('\n');
