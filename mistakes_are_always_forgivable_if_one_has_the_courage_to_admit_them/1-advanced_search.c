64898eba82e72b26393e9eb90440dcd0a357925b
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-04-07 14:39:26 -0700

    DRYing up the code

--- mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/1-advanced_search.c
+++ mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/1-advanced_search.c
@@ -7,9 +7,7 @@ int advanced_search(int *array, int size, int (*check_func)(int))
 {
   int i;
   for(i = 0; i < size; i++) {
-    if(check_func(array[i])) {
-      return i;
-    }
+    if(check_func(array[i])) return i;
   }
   return(-1);
 }
