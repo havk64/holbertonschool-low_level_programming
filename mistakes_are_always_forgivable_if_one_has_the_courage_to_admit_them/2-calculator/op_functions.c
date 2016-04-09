7299b26014c85ecea40223519e67a34d55d9fa18
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-04-09 00:08:06 -0700

    Adding comments

--- mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/2-calculator/op_functions.c
+++ mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/2-calculator/op_functions.c
@@ -1,24 +1,24 @@
-int op_add(int a, int b)
+int op_add(int a, int b) /* Function to the arithmetic sum.*/
 {
     return ( a + b );
 }
 
-int op_sub(int a, int b)
+int op_sub(int a, int b) /* Function to the subtraction.*/
 {
-    return ( a - b );
+    return ( a - b );   
 }
 
-int op_mul(int a, int b)
+int op_mul(int a, int b) /* Function to multiplication. */
 {
     return ( a * b );
 }
 
-int op_div(int a, int b)
+int op_div(int a, int b) /* Function to the division.   */
 {
     return ( a / b );
 }
 
-int op_mod(int a, int b)
+int op_mod(int a, int b) /* Function to the modulo op.  */
 {
     return ( a % b );
 }
