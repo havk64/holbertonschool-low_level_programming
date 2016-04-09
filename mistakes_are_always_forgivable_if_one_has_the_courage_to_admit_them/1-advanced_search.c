219666e6938ea2fd2faa9c0ebfcbb42641783170
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-04-08 19:52:13 -0700

    Indenting K&R style

--- mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/1-advanced_search.c
+++ mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/1-advanced_search.c
@@ -5,9 +5,9 @@
  * ============================================================= */
 int advanced_search(int *array, int size, int (*check_func)(int))
 {
-  int i;
-  for(i = 0; i < size; i++) {
-    if(check_func(array[i])) return i;
-  }
-  return(-1);
+    int i;
+    for(i = 0; i < size; i++) {
+	if(check_func(array[i])) return i;
+    }
+    return(-1);
 }
