470d75a605711f4caad73efc1b0cf2706945e6c5
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-04-08 23:52:22 -0700

    Adding comments, improving the output

--- mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/2-calculator/main.c
+++ mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/2-calculator/main.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
 #include "my_functions.h"
 
-int main(int __attribute__((unused)) argc, char *argv[]){
-    if ( *get_op_func(*argv[2]) == (NULL)){
-	return 1;
-    }
+/* ======================================================================= *
+ *    This function parse the command-line arguments to retrieve the       * 
+ *    operands and the operator                                            *
+ * ======================================================================= */
+int main(int argc, char *argv[]){
+    if (argc != 4 || *get_op_func(*argv[2]) == (NULL)) return 1;
     printf("%d\n", (*get_op_func(*argv[2]))(atoi(argv[1]), atoi(argv[3])));
     return 0;
 }
