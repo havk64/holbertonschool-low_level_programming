e7e998e4a3e6d6bf98ac6bca4a369d2936732de0
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-04-12 14:58:09 -0700

    Adding comments to task 0

--- float_like_a_butterfly_sting_like_a_bee/0-string_dup.c
+++ float_like_a_butterfly_sting_like_a_bee/0-string_dup.c
@@ -1,15 +1,21 @@
 #include <stdlib.h>
 
+/* ==================================================== *
+ *  This function returns returns a pointer to a newly  *
+ *  allocated space in memory, which contains a copy of *
+ *  the string given as a paramete                      *
+ *  by Alexandro de Oliveira, for Holberton School.     *
+ * ==================================================== */
 char *string_dup(char *str)
 {
     int i = 0;
     char *s;
-    while(*(str + i++));
+    while(*(str + i++)); /* Counting the length of the string */
     
     s = malloc(sizeof(str) * i);
     if(s != NULL) {
 	while(i--){
-	    s[i] = str[i];
+    s[i] = str[i]; /*Copying the string to reserved space */
 	}
 	return s;
     }
