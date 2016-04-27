1a3575149a08d8f0374f33e0e52a4448add0c1d0
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-04-26 18:52:21 -0700

    Adding comments to task 4

--- cisdoublefun_basics/4-positive_or_not.c
+++ cisdoublefun_basics/4-positive_or_not.c
@@ -1,4 +1,10 @@
 int print_char(char c);
+/*  ========================================== *
+ *  This function takes an integer as argument *
+ *  and prints P if the number is positive, N  *
+ *  if the number is negative, and Z if the    *
+ *  number is zero.			       *
+ *  ========================================== */
 void positive_or_not(int n)
 {
     if(n == 0) print_char('Z');
