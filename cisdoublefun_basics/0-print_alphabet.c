3ad20fa2fe523045af80fceb5226b3f334399a00
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-04-26 17:39:44 -0700

    Adding comment to task 0

--- cisdoublefun_basics/0-print_alphabet.c
+++ cisdoublefun_basics/0-print_alphabet.c
@@ -1,4 +1,8 @@
 int print_char(char c);
+/*  =======================================  *
+ *  This function prints each character of   *
+ *  the alphabet in lowercase		     *
+ *  ======================================== */
 void print_alphabet(void)
 {
     int i;
