e3724f1bf3976e7b3d1301ffc167c97350a0073b
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-05 15:06:26 -0700

    Improving code efficiency, adding comments

--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/3-string_ncopy.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/3-string_ncopy.c
@@ -6,11 +6,19 @@ char *string_ncopy(char *dest, const char *src, int n)
 {
     char *s;
     s = dest;
-    while(n && *src) {
-	*dest++ = *src++;
-	n--;
-    }
+    while(n && (*dest++ = *src++) ) /* while n and second   */
+	n--;			    /* expression are != 0  */
     while(n--)
 	*dest++ = 0;
     return s;
 }
+
+/* A longer version of this function would be:
+ *
+ * while((n != 0) && (src != 0) {
+ *	*dest = *src;
+ *	dest++;
+ *	src++;
+ *	n--;
+ * }
+ */
