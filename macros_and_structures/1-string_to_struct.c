bbe80e1d02219aeda1fcecc3584c58d2b9698e9f
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-06 07:03:03 -0700

    Improving syntax, control flow

--- macros_and_structures/1-string_to_struct.c
+++ macros_and_structures/1-string_to_struct.c
@@ -6,9 +6,10 @@
  */
 int len(char *s) {
     int i = 0 ;
-    while(*(s + i)) {
+
+    while(*(s + i))
 	i++;
-    }
+
     return i;
 }
     
@@ -18,18 +19,20 @@ int len(char *s) {
  */
 struct String *string_to_struct(char *str)
 {
-    int i;
+    int i, length;
     char *m;
     struct String s; /* Initializing the variable */
     struct String *p;
-    p = &s;	     /* Asigns the addres of the variable to the pointer.*/
-    m = malloc(len(str) * (sizeof(char) + 1)); /* Allocating resources   */
+    p = &s;	     /* Assigns the address of the variable to the pointer.*/
+    length = len(str);
+
+    m = malloc(length * (sizeof(char) + 1)); /* Allocating resources   */
     if(m != NULL) {
-	for(i = 0; i < len(str); i++) {
+	for(i = 0; i < length; i++) 
 	    m[i] = str[i]; /* Copying the string to str property.	 */ 
-	}
+	
 	m[i] = 0;	   /* The string needs to be NULL terminated. 	 */
-	p->length = len(str); /* Asigning the lenght and string to objc. */
+	p->length = length; /* Assigning the lenght and string to objc. */
 	p->str = m;
 	return p;
     }
