cf6acd743360d0aae5941c58665c2b88f6ddd935
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-06 13:21:14 -0700

    Task 3 completed

---
+++ macros_and_structures/3-free_string_struct.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "str_struct.h"
+
+/*
+ * This function frees the allocated memory of a struct
+ * given as parameter.
+ */
+void free_string_struct(struct String *str)
+{
+	free(str->str);
+	free(str);
+}
