b660d07ea23fc608ed315b84d3977e4cbbd7ca42
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-06 08:27:19 -0700

    INT_MIN

--- it_s_the_little_details_that_are_vital_little_things_make_big_things_happen/src/print_number.c
+++ it_s_the_little_details_that_are_vital_little_things_make_big_things_happen/src/print_number.c
@@ -1,33 +1,44 @@
-#include "holbertonschool.h"
-/*=> This function that takes an integer in parameter and prints it.*/
+#define INT_MIN -2147483648
+
+void print(int n);
+int print_char(char c);
+
+/*
+ * This function that takes an integer as parameter and prints each digit.   *
+ */
 void print_number(int n) /*=> Control flow to use function print accordingly */
 {  
-	if (n == 0){     /*=> If n is zero prints zero */
-       		print_char(48);
-	}
-	else if( n > 0){
-        	print(n);
-	}
-	else {		/*=> If n is negative converts to absolute value */
-		print_char('-');
-		n = -n;
-		print(n); 
-	}
+    if (n == 0)				       /*=> If n is zero prints zero */
+	print_char('0');
+	
+    else if( n >= 0)
+	print(n);
 
+    else if(n == INT_MIN){
+	print_char('-');
+	n /= 10; n = -n;
+	print(n);
+	print_char(((n + 4) % 10) + '0');
+    }
+
+    else {		    /*=> If n is negative converts to absolute value */
+	print_char('-');
+	n = -n;
+	print(n); 
+    }
 }
-void print(int n)        /*=> Uses the print_char to print each digit */
+
+void print(int n)		 /*=> Uses the print_char to print each digit */
 {
-	int number=n;
-	int power=1;
-	int print;
-	while (number > 9){  /*=> Finds the lenght of decimal  */
-		number /= 10;
-		power *= 10;
-	}
-	while (n > 0){ 	 /*=> Finds each digit and prints it with print_char */
-		print = n / power;
-		n %= power;
-		power /= 10;
-		print_char(print + 48);
-	}
+    int digit, number = n, power = 1;
+
+    while (number /= 10)		/*=> Finds the lenght of decimal      */
+	power *= 10;
+
+    while (n){		  /*=> Finds each digit and prints it with print_char */
+	digit = n / power;
+	n %= power;
+	power /= 10;
+	print_char(digit + '0');
+    }
 }
