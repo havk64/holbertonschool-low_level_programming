e19794434421ed6658a3b9afc53004d52920cb78
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-05 19:28:34 -0700

    Adding comments to task 2

--- macros_and_structures/2-print_string_struct.c
+++ macros_and_structures/2-print_string_struct.c
@@ -1,6 +1,9 @@
 #include "str_struct.h"
 int print_char(char c);
-
+/*
+ * This function finds each digit and prints it using
+ * the function print_char.
+ */
 void print(int n)
 {
     int i, p, digit;
@@ -16,15 +19,17 @@ void print(int n)
         print_char(digit + '0');
     }
 }
-
+/*
+ * This function prints the content of a struct String.     *
+ */
 void print_string_struct(struct String *str)
 {
     int i;
     for(i = 0; i < str->length; i++){
-	print_char(str->str[i]);
+	print_char(str->str[i]); /* Could use (*str) too    */
     }
-    print_char(',');
+    print_char(','); 
     print_char(' ');
-    print(str->length);
-    print_char('\n');
+    print(str->length); /* Uses the function print to print */
+    print_char('\n');   /* digit of length of string.	    */
 }
