c249315e3530853fbf2d16ea6d3e2106f1deda83
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-05 18:48:51 -0700

    Adding comments

--- macros_and_structures/1-string_to_struct.c
+++ macros_and_structures/1-string_to_struct.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include "str_struct.h"
 
+/*
+ * Function to find the lenght of a string.
+ */
 int len(char *s) {
     int i = 0 ;
     while(*(s + i)) {
@@ -9,21 +12,24 @@ int len(char *s) {
     return i;
 }
     
-
+/*
+ * This function stores a string and its length in a newly allocated
+ * structure,  and return its address.
+ */
 struct String *string_to_struct(char *str)
 {
     int i;
     char *m;
-    struct String s;
+    struct String s; /* Initializing the variable */
     struct String *p;
-    p = &s;
-    m = malloc(len(str) * (sizeof(char) + 1));
+    p = &s;	     /* Asigns the addres of the variable to the pointer.*/
+    m = malloc(len(str) * (sizeof(char) + 1)); /* Allocating resources   */
     if(m != NULL) {
 	for(i = 0; i < len(str); i++) {
-	    m[i] = str[i];
+	    m[i] = str[i]; /* Copying the string to str property.	 */ 
 	}
-	m[i] = 0;
-	p->length = len(str);
+	m[i] = 0;	   /* The string needs to be NULL terminated. 	 */
+	p->length = len(str); /* Asigning the lenght and string to objc. */
 	p->str = m;
 	return p;
     }
