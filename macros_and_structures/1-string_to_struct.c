3b2190363e87ea00d95cf3be77fae17503b90fcd
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-07 14:29:43 -0700

    Improving comments, variables

--- macros_and_structures/1-string_to_struct.c
+++ macros_and_structures/1-string_to_struct.c
@@ -1,30 +1,30 @@
 #include <stdlib.h>
 #include "str_struct.h"
 
-/*
- * This function stores a string and its length in a newly allocated
- * structure,  and return its address.
- */
+/* ==================================================================== *
+ * This function stores a string and its length in a newly allocated	*
+ * structure,  and return its address.					*
+ * ==================================================================== */
 struct String *string_to_struct(char *str)
 {
     int i, len;
-    char *m;
-    struct String *p; /* Initializing the variable */
+    char *s;
+    struct String *p;		       /* Creating pointer to struct.	*/
     p = malloc(sizeof(struct String)); /* Allocating memory to pointer	*/
     
     len = 0;
     while(*(str + len)) /* Finding the length of the string.		*/
 	len++;
     
-    m = malloc(len * (sizeof(char) + 1)); /* Allocating resources	*/
-    if(m != NULL) {
+    s = malloc(len * (sizeof(char) + 1)); /* Allocating resources	*/
+    if(s != NULL) {
 
 	for(i = 0; i < len; i++) 
-	    m[i] = str[i]; /* Copying the string to str property.	*/ 
-	m[i] = 0;	   /* The string needs to be NULL terminated.	*/
+	    s[i] = str[i]; /* Copying the string to str property.	*/ 
+	s[i] = 0;	   /* The string needs to be NULL terminated.	*/
 
 	if(p != NULL) {
-	    p->str = m;	
+	    p->str = s;	
 	    p->length = len; /*Assigning the lenght and string to objc.	*/
 	}
 	return p;
