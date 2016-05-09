db8dd993a56167177490a7e4088f670a90ff54a4
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-08 22:06:29 -0700

    Adding comments to task 0

--- linked_lists/0-add_node.c
+++ linked_lists/0-add_node.c
@@ -1,6 +1,9 @@
 #include "list.h"
 #include <stdlib.h>
 
+/* ================================================== *
+ * Allocates a new node to a linked list and links it.*
+ * ================================================== */
 int add_node(List **list, char *content)
 {
     int len, i;
@@ -11,19 +14,19 @@ int add_node(List **list, char *content)
     node = malloc(sizeof(List));
     if(node == NULL) return (1);
 
-    while(content[len])
+    while(content[len]) /* String counter.	*/
 	len++;
 
     s = malloc(sizeof(char) * (len + 1));
     if( s == NULL) return (1);
 
-    for(i = 0; i < len; i++)
+    for(i = 0; i < len; i++) /* String copy.	*/
 	s[i] = content[i];
     s[i] = 0;
 
-    node->str = s;
+    node->str = s;	/* Initializing members */
     node->next = *list;
-    *list = node;
+    *list = node;	/* Linking to next mem. */
 
     return (0);
 }
