3634422601bb51d9dd8ebcae799905cc96aa7f33
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-10 16:43:13 -0700

    Adding comments task 2

--- file_descriptors/2-print_permissions/main.c
+++ file_descriptors/2-print_permissions/main.c
@@ -2,9 +2,13 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+/* ===========================================================	*
+ *  Task 2 - File Descriptors.					*
+ *  The program prints the permissions of a file.		*
+ * ===========================================================	*/
 int print_char(char c);
 void  print(struct stat statFile)
-{
+{	/* Function to prints each item of permision.		 */
     print_char(S_ISDIR(statFile.st_mode)        ? 'd' : '-');
     print_char((statFile.st_mode & S_IRUSR)     ? 'r' : '-');
     print_char((statFile.st_mode & S_IWUSR)     ? 'w' : '-');
@@ -23,11 +27,11 @@ int main(int argc, char *argv[])
 {
     struct stat statFile;
 
-    if(argc != 2)
+    if(argc != 2)	/*	Checking the parameters.	*/
 	return (1);
 
-    if(lstat(argv[1], &statFile) == -1) return 1;
-    print(statFile);
+    if(lstat(argv[1], &statFile) == -1) return 1; /*Using lstat.*/
+    print(statFile);	/*	Call to the print function.	 */
 
     return (0);
 }
