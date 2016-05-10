7e70ea74b1239ccea8fe2f8b0143f0e99075b4ac
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-10 16:02:14 -0700

    Separate function to print

--- file_descriptors/2-print_permissions/main.c
+++ file_descriptors/2-print_permissions/main.c
@@ -4,6 +4,21 @@
 #include "header.h"
 
 int print_char(char c);
+void  print(struct stat statFile)
+{
+    print_char(S_ISDIR(statFile.st_mode)        ? 'd' : '-');
+    print_char((statFile.st_mode & S_IRUSR)     ? 'r' : '-');
+    print_char((statFile.st_mode & S_IWUSR)     ? 'w' : '-');
+    print_char((statFile.st_mode & S_IXUSR)     ? 'x' : '-');
+    print_char((statFile.st_mode & S_IRGRP)     ? 'r' : '-');
+    print_char((statFile.st_mode & S_IWGRP)     ? 'w' : '-');
+    print_char((statFile.st_mode & S_IXGRP)     ? 'x' : '-');
+    print_char((statFile.st_mode & S_IROTH)     ? 'r' : '-');
+    print_char((statFile.st_mode & S_IWOTH)     ? 'w' : '-');
+    print_char((statFile.st_mode & S_IXOTH)     ? 'x' : '-');
+    print_char('\n');
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -13,17 +28,7 @@ int main(int argc, char *argv[])
 	return (1);
 
     if(lstat(argv[1], &statFile) == -1) return 1;
+    print(statFile);
 
-    print_char(S_ISDIR(statFile.st_mode)	? 'd' : '-');
-    print_char((statFile.st_mode & S_IRUSR)	? 'r' : '-');
-    print_char((statFile.st_mode & S_IWUSR)	? 'w' : '-');
-    print_char((statFile.st_mode & S_IXUSR)	? 'x' : '-');
-    print_char((statFile.st_mode & S_IRGRP)	? 'r' : '-');
-    print_char((statFile.st_mode & S_IWGRP)	? 'w' : '-');
-    print_char((statFile.st_mode & S_IXGRP)	? 'x' : '-');
-    print_char((statFile.st_mode & S_IROTH)	? 'r' : '-');
-    print_char((statFile.st_mode & S_IWOTH)	? 'w' : '-');
-    print_char((statFile.st_mode & S_IXOTH)	? 'x' : '-');
-    print_char('\n');	       
     return (0);
 }
