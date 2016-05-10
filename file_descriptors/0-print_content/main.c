8e7ab7eef8f71fd68e75cd4ecf80df6f4eda641e
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-09 17:20:23 -0700

    Adding comments to task 0

--- file_descriptors/0-print_content/main.c
+++ file_descriptors/0-print_content/main.c
@@ -1,7 +1,11 @@
 #include <fcntl.h>
 #include <unistd.h> 
 
-int print_char(int c)
+/* ==================================================== *
+ *  Task 0 - File Descriptors. Read from a file, prints *
+ *  to standard output.					*
+ * ====================================================	*/
+int print_char(int c) /* Print Char function.		*/
 {
     return(write(1, &c, 1));
 }
@@ -9,15 +13,15 @@ int print_char(int c)
 int main(int argc, char *argv[])
 {
     int file, stream;
-    if(argc != 2) 
+    if(argc != 2)  /* If no argument, return 1.		*/
 	return (1);
 
-    file = open(argv[1],O_RDONLY);
+    file = open(argv[1],O_RDONLY); /* Open the file.	*/
 
-    while(read(file, &stream, 1))
+    while(read(file, &stream, 1)) /* reading each char. */
 	print_char(stream);
 
-    if(close(file)) return (1);
+    if(close(file)) return (1); /* Closing the file.	*/
     
     return (0);
 }
