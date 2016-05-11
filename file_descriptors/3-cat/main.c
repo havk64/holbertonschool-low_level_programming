15d18e6aeade296471837f7c2b212bce7b6fb36f
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-10 22:42:30 -0700

    Fixing reference

--- file_descriptors/3-cat/main.c
+++ file_descriptors/3-cat/main.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include "header.h"
 
-int logIt(int fd, char *bf[])
+int logIt(int fd, char bf[])
 {
     int in, out;
     
@@ -16,27 +16,17 @@ int logIt(int fd, char *bf[])
 int main(int ac, char *av[])
 {
     int i, fd, ret;
-    char *bf[BUF_SIZE];
+    char bf[BUF_SIZE];
     
-/*
-    if(ac > 2) {
-	for(i = 0; i < ac; i++) {
-	    if(read(argv[i], O_RDONLY) != -1) {
-	    */
     if(ac > 1) {
 	for(i = 1; i < ac; i++) {
 	    if((fd = open(av[i], O_RDONLY)) == -1) return (1);
 	    ret = logIt(fd, bf);
 	    if((close(fd) == -1) || (ret == 1 )) return (1);
 	}
-/*	while((in = read(fd, bf, BUF_SIZE))) {
-	    out = write(1, bf, in);
-	    if(out != in) return (1);
-	    }*/
     }
     else
 	return logIt(0, bf);
-
 
     return (0);
 }
