b591f644358330b0b875f9feebe9e382d5a2c7df
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-11 02:02:19 -0700

    Adding comments task 4

--- linked_lists/3-print_list.c
+++ linked_lists/3-print_list.c
@@ -1,11 +1,14 @@
 #include "list.h"
 
+/*
+ * This function prints each element of a list.	*
+ */
 void print_list(List *list)
 {
     List *node;
     node = list;
     while(node != NULL) {
-	print(node->str);
+	print(node->str);/* External function. */
 	if(node->next != NULL) {
 	    print_char(',');
 	    print_char(' ');
