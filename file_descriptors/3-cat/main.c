bc208742d14b7c5cfefdf897cc62e2978a129eb1
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-10 22:55:49 -0700

    Adding comments

--- file_descriptors/3-cat/main.c
+++ file_descriptors/3-cat/main.c
@@ -2,12 +2,16 @@
 #include <unistd.h>
 #include "header.h"
 
+/* ===========================================================  *
+ *  Task 3 - File Descriptors.                                  *
+ *  The "Cat clone", reads from stdin, prints to stdout.	*
+ * ===========================================================  */
 int logIt(int fd, char bf[])
-{
+{			/* Separate function to print to stdout */
     int in, out;
     
-    while((in = read(fd, bf, BUF_SIZE))) {
-	out = write(1, bf, in);
+    while((in = read(fd, bf, BUF_SIZE))) { /* Reads 8192 bits.	*/
+	out = write(1, bf, in); /*	Writes what it reads.	*/
 	if(out != in) return (1);
     }
     return (0);
@@ -21,11 +25,11 @@ int main(int ac, char *av[])
     if(ac > 1) {
 	for(i = 1; i < ac; i++) {
 	    if((fd = open(av[i], O_RDONLY)) == -1) return (1);
-	    ret = logIt(fd, bf);
+	    ret = logIt(fd, bf); /* Gets the return of function */
 	    if((close(fd) == -1) || (ret == 1 )) return (1);
-	}
+	}     /* ^^^	   Closing the file(s).			*/
     }
-    else
+    else	/* if no parameters are given, reads from stdin.*/
 	return logIt(0, bf);
 
     return (0);
