4eb9abbb4f11c7dcdafd1cad5174f2cf3f8ea5b7
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-11 00:38:14 -0700

    Using char instead of int

--- file_descriptors/0-print_content/main.c
+++ file_descriptors/0-print_content/main.c
@@ -12,11 +12,14 @@ int print_char(int c) /* Print Char function.		*/
 
 int main(int argc, char *argv[])
 {
-    int file, stream;
+    int file;
+    char stream;
+
     if(argc != 2)  /* If no argument, return 1.		*/
 	return (1);
 
     file = open(argv[1],O_RDONLY); /* Open the file.	*/
+    if(file == -1) return (1);
 
     while(read(file, &stream, 1)) /* reading each char. */
 	print_char(stream);
