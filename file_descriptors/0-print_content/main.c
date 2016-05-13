e23721d703ba0ab483aff07309f7f3d5ce30abb3
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-12 18:57:10 -0700

    Increasing read buffer

--- file_descriptors/0-print_content/main.c
+++ file_descriptors/0-print_content/main.c
@@ -7,8 +7,8 @@
 
 int main(int argc, char *argv[])
 {
-    int file;
-    char c;
+    int file, in;
+    char c[BUF_SIZE];
 
     if(argc != 2)  /* If no argument, return 1.		*/
 	return (1);
@@ -16,8 +16,8 @@ int main(int argc, char *argv[])
     file = open(argv[1],O_RDONLY); /* Open the file.	*/
     if(file == -1) return (1);
 
-    while(read(file, &c, 1)) /* reading each char.	*/
-	print_char(c);
+    while((in = read(file, c, BUF_SIZE))) /* reading each char.	*/
+	write(1, c, in);
 
     if(close(file)) return (1); /* Closing the file.	*/
     
