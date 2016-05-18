10b459b5086060f8c45d4b7677650ab771219065
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-18 13:16:17 -0700

    Implementing strncpy

--- simple_shell/src/strings.c
+++ simple_shell/src/strings.c
@@ -1,6 +1,20 @@
 #include "shell.h"
 
 /*
+ *  Implementation of strncmp() 
+ */
+int strncomp(char *s1, const char *s2, int n)
+{
+    while(*s1 == *s2 && n != 0) {
+        if(*s1 == 0) {
+            return 0;
+        }
+        s1++; s2++; n--;
+    }
+    return *(const unsigned char *)s1 - *(const unsigned char *)s2;
+}
+
+/*
  *  Implementation of strcmp()
  */
 int strcomp(char *s1, const char *s2)
@@ -26,3 +40,29 @@ int len(char *s)
 
     return i;
 }
+
+/*
+ * Implementation of read_line()
+ *
+char *read_line(void)
+{
+    char *buff; 
+    int i = 0;
+    int c;
+
+    if((buff =  malloc(sizeof(char) * BUF_SIZE + 1))) {
+	perror("Allocation failed!");
+	return 1;
+    }
+
+    c = getchar();
+
+    if(c == EOF || c == '\n') {
+	buff[i] = 0;
+	return buff;
+    } else 
+	buff[i] = c;
+    
+    i++;
+}
+*/
