63f555fac8f715022a78b52d39a1c5db2904e6d1
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-18 19:40:15 -0700

    Adding function to parse env variables

--- simple_shell/src/parse.c
+++ simple_shell/src/parse.c
@@ -13,3 +13,15 @@ char *getPath(char ** env)
 
     return NULL;
 }
+
+char *getEnv(char * name, char ** env)
+{
+    int i, l;
+    l = len(name);
+
+    for(i = 0; env[i]; i++)
+        if(strncomp(name, env[i], l) == 0)
+            return (env[i] + l + 1);
+
+    return NULL;
+}
