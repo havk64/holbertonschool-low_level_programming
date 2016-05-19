db1eecd7b61015ea80c3c8e68c18834f026e0143
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-19 16:52:40 -0700

    Fixing commandExist function

--- simple_shell/src/parse.c
+++ simple_shell/src/parse.c
@@ -28,25 +28,26 @@ char *commandExist(char * cmd, char ** env)
     char ** paths;
     struct stat st;
 
-    if((command = malloc(sizeof(char) * BUF )) == NULL)
+    if((command = malloc(sizeof(char) * 32 )) == NULL)
 	return NULL;
 
     path = getEnv("PATH", env);
     paths = string_split(path, ':');
 
     for( i = 0; paths[i] != 0; i++) {
-        string_ncopy(command, paths[i], len(paths[i]) + 1);
-        nconcat_strings(command, "/", 1);
+        string_ncopy(command, paths[i], (len(paths[i]) + 1));
+        nconcat_strings(command, "/", 2);
         nconcat_strings(command, cmd, len(cmd) + 1);
         printf("Inside for: %s\n", command);
-        if(stat(command, &st) == 0)
+        if(stat(command, &st) == 0) {
+	    freeMem(paths);
+	    free(paths);
             return command;
-        else
-            free(command);
+	}        
     }
     freeMem(paths);
     free(paths);
-    return NULL;
+    return command;
 }
 
 /* 
