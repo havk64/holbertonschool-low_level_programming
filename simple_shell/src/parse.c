5d31836697a88a8c17d6125ae94c1af6a70e4036
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-19 00:03:04 -0700

    Adding function to parse the PATH

--- simple_shell/src/parse.c
+++ simple_shell/src/parse.c
@@ -1,19 +1,29 @@
 #include "shell.h"
 
-char *getPath(char ** env)
+int parseIt(char * env[])
 {
-    int i, l;
-    char * path;
-    path = "PATH";
-    l = len(path);
+    int l;
+    char *p, *p1;
+    char ** path;
+    
+    p = getEnv("PATH", env);
+    l = len(p);
+    
+    p1 = malloc(sizeof(char) * l + 1);
+    if(p1 == NULL) return 1;
+    strcpy(p1, p);
+    path = string_split(p2, ':');
+    
+    freeMem(path);
+    free(path);
+    free(p1);
 
-    for(i = 0; env[i]; i++)
-        if(strncomp(path, env[i], l) == 0)
-            return (env[i] + l + 1);
-
-    return NULL;
+    return (0);
 }
 
+/* 
+ * Function getEnv returns a pointer to the value of a env variable.
+ */
 char *getEnv(char * name, char ** env)
 {
     int i, l;
