e6b905dbce8661895d77f53e96bbc2a4ac0fc3f7
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-18 23:48:42 -0700

    Adding implementation of strcpy

--- simple_shell/src/strings.c
+++ simple_shell/src/strings.c
@@ -42,3 +42,12 @@ int len(char *s)
 
     return i;
 }
+
+char *string_copy(char *dest, const char *src)
+{
+    char *s;
+    s = dest;
+    while((*dest++ = *src++)) ; /* Empty while body */
+/*    *dest = 0;*/
+    return s;
+}
