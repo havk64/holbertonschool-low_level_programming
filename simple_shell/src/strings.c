c839de830ff18c4b95d6c77a50f8e4c8b3275336
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-05-20 17:53:26 -0700

    Adding function to concatenate strings

--- simple_shell/src/strings.c
+++ simple_shell/src/strings.c
@@ -1,4 +1,28 @@
 #include "shell.h"
+/*
+ *      This function concatenate two strings allocating
+ *	enough space, and returns its pointer.
+ */ 
+     char *string_concat(char *s1, char *s2)
+ {
+     int i = 0;
+     char *s;
+     int sum = len(s1) + len(s2);
+     s = malloc(sizeof(char) * (sum + 1 ));
+     if(s != NULL) {
+	 while(i < len(s1)) {
+	     s[i] = s1[i];
+	     i++;
+	 }
+	 while(i < sum) {
+	     s[i] = s2[i - len(s1)];
+	     i++;
+	 }
+	 s[i] = 0;
+	 return s;
+     }
+     return NULL;
+ }
 
 /*
  *  Implementation of strncmp() 
