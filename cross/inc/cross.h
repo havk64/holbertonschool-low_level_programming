4e544b4485c8de0284e3d88c3dedabacbfeb11de
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-06-18 22:55:49 -0700

    Including header file

---
+++ cross/inc/cross.h
@@ -0,0 +1,9 @@
+#include <unistd.h>
+
+int print_char(char c);
+int isNum(char *str);
+int len(char * s);
+void print(char * s);
+void printX(char *s);
+int atoi(char * s);
+void print_line(int,int);
