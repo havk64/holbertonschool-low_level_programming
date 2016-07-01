427e5008f6d7c459347582c04128b7e97297664d
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-06-30 23:48:59 -0700

    Task 7 completed

---
+++ advanced_linked_lists/circular_linked_list/free_cl_list.c
@@ -0,0 +1,17 @@
+#include "header.c"
+
+void free_cl_list(List * list) {
+    List * head = list;
+    List * node = list;
+
+    if (list == NULL) return;
+
+    while (list->next != head) {
+	node = list;
+	list = list->next;
+	free(node->str);
+	free(node);
+    }
+    free(list->str);
+    free(list);
+}
