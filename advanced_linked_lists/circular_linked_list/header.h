db4ca75dd2beb5cb5b58b0a3308865132753465d
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-06-30 23:35:18 -0700

    Adding header file

---
+++ advanced_linked_lists/circular_linked_list/header.h
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "list.h"
+
+void print_string(char *s);
+int print_char(char c);
+int add_end_cl_list(List **list, char *str);
+
