458ed223448251a877eb36951bd456a46444e6c2
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-07-01 00:00:46 -0700

    Adding func prototype

--- advanced_linked_lists/circular_linked_list/header.h
+++ advanced_linked_lists/circular_linked_list/header.h
@@ -6,6 +6,7 @@
 void print_string(char *s);
 int print_char(char c);
 int add_end_cl_list(List **list, char *str);
+int add_begin_cl_list(List ** list, char * str);
 void free_cl_list(List * list);
 void print_cl_list(List *list);
 List * array_to_cl_list(char ** array);
