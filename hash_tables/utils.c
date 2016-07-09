948dc221ed9608f9bed8288b5287719c224ddeaf
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-07-08 20:53:11 -0700

    Adding helper functions source file

---
+++ hash_tables/utils.c
@@ -0,0 +1,12 @@
+#include "header.h"
+
+int compare(const char * a, const char * b)
+{
+	while(*a == *b) {
+		if(*a == '\0' && *b == '\0') {
+			return 0;
+		}
+		a++; b++;
+	}
+	return *(const unsigned char *)a - *(const unsigned char *)b;
+}
