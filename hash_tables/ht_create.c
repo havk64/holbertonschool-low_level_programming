91fc4ae487aec455cd32e117d001a78996673b9b
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-07-08 20:48:54 -0700

    Task 1 completed

--- hash_tables/ht_create.c
+++ hash_tables/ht_create.c
@@ -3,15 +3,20 @@
 
 HashTable *ht_create(unsigned int size)
 {
+	unsigned int i;
 	HashTable * ht;
 	List ** array;
 	
+	if(size < 1) return NULL;
+	
 	ht = malloc(sizeof(HashTable));
-	if(ht == NULL) return 1;
+	if(ht == NULL) return NULL;
 	
 	array = malloc(sizeof(List) * size);
-	if(array == NULL) return 1;
+	if(array == NULL) return NULL;
 	
+	for(i = 0; i < size; i++) array[i] = NULL;
+
 	ht->size = size;
 	ht->array = array;
 
