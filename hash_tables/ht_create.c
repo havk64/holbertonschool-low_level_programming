dc411749667a288974578640f1c0672532922fa9
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-07-29 21:10:17 -0700

    Changes to meet C Kernel style requirements

--- hash_tables/ht_create.c
+++ hash_tables/ht_create.c
@@ -1,6 +1,5 @@
 #include "header.h"
-
-/* 
+/**
  *	This function creates a hash table.
  * 	@size	:= the size of the hash table.
  * 	@return	:= a Hash Table.
@@ -8,23 +7,21 @@
 HashTable *ht_create(unsigned int size)
 {
 	unsigned int i;
-	HashTable * ht;
-	List ** array;
-	
-	if(size < 1) return NULL;
-	
+	HashTable *ht;
+	List **array;
+	if (size < 1)
+		return (NULL);
 	ht = malloc(sizeof(HashTable));
-	if(ht == NULL) return NULL;
-	
+	if (ht == NULL)
+	       return (NULL);
 	array = malloc(sizeof(List) * size);
-	if(array == NULL) return NULL;
-	
-	for(i = 0; i < size; i++) array[i] = NULL;
-
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
 	ht->size = size;
 	ht->array = array;
-
-	return ht;
+	return (ht);
 }
 
 
