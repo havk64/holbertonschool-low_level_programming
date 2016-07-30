a445840ea1a21fed34a6a38acd055bccfff705cf
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-07-29 21:41:16 -0700

    Changes to meet Linux Kernel coding style

--- hash_tables/ht_put.c
+++ hash_tables/ht_put.c
@@ -1,35 +1,35 @@
 #include "header.h"
-
-/* 
- * 	This function add an element to a hash table.
- * 	@hashtable	:= the hash table to be filled.
- * 	@key		:= the key of the key-value pair.
- * 	@value		:= the value of the key-value pair. 
+/**
+ * This function add an element to a hash table.
+ * @hashtable	:= the hash table to be filled.
+ * @key		:= the key of the key-value pair.
+ * @value	:= the value of the key-value pair.
  */
 int ht_put(HashTable *hashtable, const char *key, const char *value)
 {
 	unsigned int index;
-	List * list;
-	if(key == NULL) return 1;
+	List *list;
 
+	if (key == NULL)
+		return (1);
 	index = hash(key, hashtable->size);
 	list = hashtable->array[index];
-	while(list != NULL) {
-		if(compare(list->key, key) == 0) {
+	while (list != NULL)
+	{
+		if (compare(list->key, key) == 0)
+		{
 			free(list->value);
 			list->value = strdup(value);
-			return 0;
+			return (0);
 		}
 		list = list->next;
 	}
-
 	list = malloc(sizeof(List));
-	if(list == NULL) return 1;
-	
+	if (list == NULL)
+		return (1);
 	list->key = strdup(key);
 	list->value = strdup(value);
 	list->next = hashtable->array[index];
-
 	hashtable->array[index] = list;
-	return 0;
+	return (0);
 }
