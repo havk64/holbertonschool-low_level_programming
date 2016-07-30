d5e5963a775f1b7c9fb08e0bf03e21bbcbd00e6b
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-07-29 21:30:08 -0700

    Changes to meet Linux Kernel coding style

--- hash_tables/ht_get.c
+++ hash_tables/ht_get.c
@@ -1,24 +1,23 @@
 #include "header.h"
-
-/* 
- * 	This function gets the key-value pair from a hash table.
- * 	@hashtable	:= the hash table.
- * 	@key		:= the key from where we will retrive the value.
+/**
+ * This function gets the key-value pair from a hash table.
+ * @hashtable	:= the hash table.
+ * @key		:= the key from where we will retrive the value.
  */
 char *ht_get(HashTable *hashtable, const char *key)
 {
 	unsigned int index;
-	List * bucket;
+	List *bucket;
 
-	if(key == NULL) return NULL;
-
+	if (key == NULL)
+		return (NULL);
 	index = hash(key, hashtable->size);
 	bucket = hashtable->array[index];
-	while(bucket != NULL) {
-		if(compare(key, bucket->key)) {
-			return bucket->value;
-		}
+	while (bucket != NULL)
+	{
+		if (compare(key, bucket->key))
+			return (bucket->value);
 		bucket = bucket->next;
 	}
-	return NULL;
+	return (NULL);
 }
