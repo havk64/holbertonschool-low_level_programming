9e29a5ef442477c6c227abc844fdcc93934bf51c
Alexandro <alexandro.oliveira@holbertonschool.com>
2016-08-07 22:35:53 -0700

    Task 0 completed

--- fifo_lifo/push_queue.c
+++ fifo_lifo/push_queue.c
@@ -1,1 +1,28 @@
+#include "head.h"
+
+int push_queue(Queue **queue, char *str)
+{
+	Queue *node;
+	node = *queue;
+
+	if (queue != NULL) {
+		while (node->next != NULL) {
+			node = node->next;
+		}
+		node->next = (Queue *) malloc(sizeof(Queue));
+		if (node->next == NULL)
+			return (1);
+		node = node->next;
+	} else {
+		node = (Queue *) malloc(sizeof(Queue));
+		if (node->next == NULL)
+			return (1);
+		*queue = node;
+	}
+	node->next = NULL;
+	node->str = strdup(str);
+	return (0);
+}
+
+
 
